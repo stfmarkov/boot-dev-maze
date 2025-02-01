6ee6d457ee125bb264b67f4d8c05324ee772d854
stfmarkov <stf.markov@gmail.com>
2025-02-01 20:41:49 +0200

    cell grid for the maze

---
+++ classes/maze.py
@@ -0,0 +1,47 @@
+from classes.cell import Cell
+import time
+
+class Maze():
+    BUFFER = 10
+
+    def __init__(        
+        self,
+        num_rows,
+        num_cols,
+        win,
+    ):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.canvas = win.canvas
+
+        self.cell_size_x = 0
+        self.cell_size_y = 0
+
+        self.win = win
+        self.cells = []
+
+        self.canvas.after(100, self._create_cells)
+
+    def _create_cells(self):
+        self.cell_size_x = (self.canvas.winfo_width() - 2*self.BUFFER) // self.num_cols
+        self.cell_size_y = (self.canvas.winfo_height() - 2*self.BUFFER)// self.num_rows
+        for col in range(self.num_cols):
+            self.cells.append([])
+            for row in range(self.num_rows):
+                cell = Cell(self.win)
+                self.cells[col].append(cell)
+
+        self._draw_cells()
+
+    def _draw_cells(self):
+        for col in range(self.num_cols):
+            for row in range(self.num_rows):
+                x1 = col * self.cell_size_x + self.BUFFER
+                x2 = x1 + self.cell_size_x 
+                y1 = row * self.cell_size_y + self.BUFFER
+                y2 = y1 + self.cell_size_y
+                self.cells[col][row].draw(x1, x2, y1, y2)
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
--- main.py
+++ main.py
@@ -2,34 +2,37 @@ from classes.window import Window
 from classes.point import Point
 from classes.line import Line
 from classes.cell import Cell
+from classes.maze import Maze
 
 def main():
     win = Window(800, 600)
 
-    cell1 = Cell(win)
-    cell2 = Cell(win)
-    cell3 = Cell(win)
-    cell4 = Cell(win)
+    # cell1 = Cell(win)
+    # cell2 = Cell(win)
+    # cell3 = Cell(win)
+    # cell4 = Cell(win)
 
-    cell1.left_wall = False
-    cell1.right_wall = False
+    # cell1.left_wall = False
+    # cell1.right_wall = False
 
-    cell2.left_wall = False
-    cell2.bottom_wall = False
+    # cell2.left_wall = False
+    # cell2.bottom_wall = False
 
-    cell3.right_wall = False
+    # cell3.right_wall = False
 
-    cell4.left_wall = False
-    cell4.top_wall = False
+    # cell4.left_wall = False
+    # cell4.top_wall = False
 
-    cell1.draw(100, 200, 100, 200)
-    cell2.draw(200, 300, 100, 200)
-    cell3.draw(100, 200, 200, 300)
-    cell4.draw(200, 300, 200, 300)
+    # cell1.draw(100, 200, 100, 200)
+    # cell2.draw(200, 300, 100, 200)
+    # cell3.draw(100, 200, 200, 300)
+    # cell4.draw(200, 300, 200, 300)
 
-    cell1.draw_move(cell2)
-    cell2.draw_move(cell4)
-    cell4.draw_move(cell3)
+    # cell1.draw_move(cell2)
+    # cell2.draw_move(cell4)
+    # cell4.draw_move(cell3)
+
+    maze = Maze(10, 10, win)
 
     win.run()
 
